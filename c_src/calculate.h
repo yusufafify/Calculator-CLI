5420557647983773748f6d6adac3bd5bf71d8c54
hamsasaber <hamsa_saber_03@hotmail.com>
2025-06-01 17:20:56 +0300

    calculations initializations

---
+++ c_src/calculate.h
@@ -0,0 +1,6 @@
+#define CALCULATE_H
+
+double add(double num1, double num2);
+double subtract(double num1, double num2);
+double multiply(double num1, double num2);
+double divide(double num1, double num2);
