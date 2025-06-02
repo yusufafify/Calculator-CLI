2b9c600eb0b1222faaa26ba239f92663acc76902
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-06-03 02:11:28 +0300

    Remove unused C formatting script as it is no longer needed

--- scripts/format_c_code.sh
+++
@@ -1,7 +0,0 @@
-# This script formats all C source and header files in the project using clang-format.
-# It is intended to be used as a pre-commit hook or manually before pushing code.
-
-# Usage (from project root):
-#   bash scripts/format_c_code.sh
-
-find ./c_src ./tests_c -type f \( -name '*.c' -o -name '*.h' \) -exec clang-format -i --style=file {} +
