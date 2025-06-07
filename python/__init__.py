3bbdcc6077dd629d7c34076e3e18afb3912c469d
yusufafify <youssef.mansour02@eng-st.cu.edu.eg>
2025-06-07 22:10:14 +0300

    Refactor import logic for calculator_c to dynamically locate the module in site-packages

--- python/__init__.py
+++ python/__init__.py
@@ -1,4 +1,37 @@
-import calculator_c
+import sys
+import os
+import site
+
+# Try to import calculator_c, if it fails, try to find and add it to the path
+try:
+    import calculator_c
+except ImportError:
+    # Look for calculator_c in all site-packages directories
+    calculator_c_found = False
+
+    # Check virtual environment first
+    venv_path = os.environ.get("VIRTUAL_ENV")
+    if venv_path:
+        venv_site_packages = os.path.join(venv_path, "Lib", "site-packages")
+        calculator_c_path = os.path.join(venv_site_packages, "calculator_c")
+
+        if os.path.exists(calculator_c_path):
+            if venv_site_packages not in sys.path:
+                sys.path.insert(0, venv_site_packages)
+            calculator_c_found = True
+
+    # Check all site-packages directories
+    if not calculator_c_found:
+        for site_package in site.getsitepackages():
+            calculator_c_path = os.path.join(site_package, "calculator_c")
+            if os.path.exists(calculator_c_path):
+                if site_package not in sys.path:
+                    sys.path.insert(0, site_package)
+                calculator_c_found = True
+                break
+
+    # Try import again
+    import calculator_c
 
 
 class Calculator:
